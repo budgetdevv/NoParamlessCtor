fe99a2c0596f0adc27b5c73d580b9bd4ab1dab80
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2025-06-09 14:32:02 +0800

    Include sample project

---
+++ Sample/Program.cs
@@ -0,0 +1,61 @@
+﻿using NoParamlessCtor.Shared.Attributes;
+
+namespace Sample
+{
+    [NoParamlessCtor]
+    public partial struct NonPrimaryCtor
+    {
+        public string Text;
+
+        public NonPrimaryCtor(string text)
+        {
+            Text = text;
+        }
+    }
+
+    [NoParamlessCtor]
+    public partial struct PrimaryCtor(string text1, string text2)
+    {
+        public string Text1 = text1;
+
+        public string Text2 = text2;
+    }
+
+    [NoParamlessCtor]
+    public ref partial struct RefCtor(ref PrimaryCtor value)
+    {
+        public ref PrimaryCtor Value = ref value;
+    }
+
+    [NoParamlessCtor]
+    public ref partial struct InCtor(in PrimaryCtor value)
+    {
+        public ref readonly PrimaryCtor Value = ref value;
+    }
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Will generate compile error!
+
+            // var nonPrimaryCtor = new NonPrimaryCtor();
+            //
+            // var primaryCtor = new PrimaryCtor();
+            //
+            // var refCtor = new RefCtor();
+            //
+            // var inCtor = new InCtor();
+
+            // Compiles fine with parameterized constructors
+
+            var nonPrimaryCtor = new NonPrimaryCtor("Hello");
+
+            var primaryCtor = new PrimaryCtor("Hello", "World");
+
+            var refCtor = new RefCtor(ref primaryCtor);
+
+            var inCtor = new InCtor(in primaryCtor);
+        }
+    }
+}
